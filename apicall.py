10cfb0dae452b67d5582dd41845b09ebd5db2e61
radoslaw-krol <radoslaw.krol@icloud.com>
2023-05-31 13:12:05 +0200

    JSON date separation
    Every new entry will be separated by a date, so it is possible to filter data by date.

--- apicall.py
+++ apicall.py
@@ -3,6 +3,12 @@
 
 import yfinance as yf
 import json
+from datetime import date
+
+
+#Get Current Date
+current_date = date.today().strftime("%Y-%m-%d")
+
 
 msft = yf.Ticker("CL=F")
 result = msft.info
@@ -12,6 +18,7 @@ result_json = json.dumps(result, indent=4)
 
 # Append the JSON result to a file
 with open('result.json', 'a') as json_file:
+    json_file.write(f"\n\n----- {current_date} -----\n\n") # add date separation
     json_file.write(result_json)
     json_file.write('\n')  # Add a newline character for readability if desired
 
