4db481d1c8c376fc63f2011431dfb7fa128ea384
radoslaw-krol <radoslaw.krol@icloud.com>
2023-06-03 11:34:07 +0200

    New formatter for JSON file to remove uncesessary data and incorporate date into JSON structure

--- lemaire.lua
+++ lemaire.lua
@@ -6,33 +6,44 @@ local cjson = require("cjson")
 local menu = require("menu")
 local art = require("art")
 
-
 local function executeProgram()
-  
--- Read the JSON data from file
-local file = io.open("result.json", "r")
-local json_data = file:read("*a")
-file:close()
+    -- Read the JSON data from file
+    local file = io.open("result.json", "r")
+    local json_data = file:read("*a")
+    file:close()
 
--- Parse the JSON data
-local data = cjson.decode(json_data)
+    -- Remove date header
+      -- Remove the date header from the JSON data
+    local json_content = json_data:gsub("%-%-%-%-%- %d%d%d%d%-%d%d%-%d%d %-%-%-%-%-\n", "")
 
--- Access and display specific parts of the JSON
-if data and data.symbol then
-    print("Symbol: " .. data.symbol)
-else
-    print("Symbol field not found in JSON data")
+
+    -- Parse the JSON data
+    local data = cjson.decode(json_content)
+
+    -- Check if data exists and is a table
+    if type(data) == "table" then
+        -- Iterate over each date entry
+        for date, entry in pairs(data) do
+            print("----- " .. date .. " -----")
+            -- Access and display specific parts of the entry
+            if entry and entry.symbol then
+                print("Symbol: " .. entry.symbol)
+            else
+                print("Symbol field not found in JSON data")
+            end
+
+            if entry and entry.bid then
+                print("Bid: " .. entry.bid)
+            else
+                print("Bid field not found in JSON data")
+            end
+
+            -- You can access and process other properties as needed
+        end
+    else
+        print("Invalid JSON data")
+    end
 end
-
-if data and data.bid then
-    print("Bid: " .. data.bid)
-else
-    print("Bid field not found in JSON data")
-end
-
-end
-
--- Display dashboard art
 
 showArt()
 
@@ -48,7 +59,7 @@ choice = io.read("*n")
 
 if choice == 1 then
 
-  os.execute("python3 apicall.py")
+  os.execute("python3 api.py")
 elseif choice == 2 then
   print("Executing Program...")
   executeProgram()
