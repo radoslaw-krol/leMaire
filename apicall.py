8b73fca00b8e085e03529cb99f71668d72d51e87
radoslaw-krol <radoslaw.krol@icloud.com>
2023-06-03 10:32:09 +0200

    Data Checks
    revised apicall.py to integrate check if current date exists in the result.json file to avoid data duplicates.

--- apicall.py
+++ apicall.py
@@ -1,24 +1,33 @@
-#!/usr/bin/env python
-
-
 import yfinance as yf
 import json
 from datetime import date
 
-
-#Get Current Date
+# Get the current date
 current_date = date.today().strftime("%Y-%m-%d")
 
+#Define the ticker symbol variable
 
-msft = yf.Ticker("CL=F")
-result = msft.info
+ticker_symbol = "CL-F"
 
-# Convert result to JSON
-result_json = json.dumps(result, indent=4)
 
-# Append the JSON result to a file
-with open('result.json', 'a') as json_file:
-    json_file.write(f"\n\n----- {current_date} -----\n\n") # add date separation
-    json_file.write(result_json)
-    json_file.write('\n')  # Add a newline character for readability if desired
+# Perform the check and save data if necessary
+with open('result.json', 'r+') as json_file:
+    file_content = json_file.read()
+
+    # Check if the file content contains a header with the current date
+    if f"----- {current_date} -----" in file_content:
+        print(f"Data for {current_date} already exists in the file.")
+    else:
+        ticker = yf.Ticker(ticker_symbol)
+        result = ticker.info
+
+        # Convert result to JSON
+        result_json = json.dumps(result, indent=4)
+
+        # Append the JSON result to the file with the date as a separator
+        if file_content:
+            json_file.write('\n\n')
+        json_file.write(f"----- {current_date} -----\n\n")
+        json_file.write(result_json)
+        json_file.write('\n')
 
